dd790ca4666b2a2b0ae33ffdb2d493b6a73ad65b
suzuno3700@gmail.com <suzuno3700@gmail.com>
2021-02-22 19:50:31 +0900

    ValueObjectのテストの追加

---
+++ PasswordGenerator.Tests/PasswordTypeTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PasswordGenerator.Domain.ValueObjects;
+
+namespace PasswordGenerator.Tests
+{
+    [TestClass]
+    public class PasswordTypeTest
+    {
+        [TestMethod]
+        public void 同一値チェック()
+        {
+
+            Assert.IsTrue(new PasswordType(PasswordType.AlphabetValue) == PasswordType.Alphabet);
+            Assert.IsTrue(new PasswordType(PasswordType.NumberValue) == PasswordType.Number);
+
+        }
+        public void 不一致チェック()
+        {
+
+            Assert.IsTrue(new PasswordType(PasswordType.AlphabetValue) == PasswordType.Alphabet);
+            Assert.IsTrue(new PasswordType(PasswordType.NumberValue) == PasswordType.Number);
+
+        }
+    }
+}
